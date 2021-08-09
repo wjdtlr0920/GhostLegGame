9b5e23c8308501e206838367e68074c0301823ae
wjdtlr0920 <wjdtlr0920@gmail.com>
2021-08-09 18:50:17 +0900

    [김정식] 메인화면 구성중

--- app/src/main/java/com/js/ghostleggame/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/js/ghostleggame/presentation/activity/MainActivity.kt
@@ -1,36 +1,92 @@
 package com.js.ghostleggame.presentation.activity
 
+import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.Window
+import android.widget.Button
+import android.widget.NumberPicker
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.js.ghostleggame.R
-import com.js.ghostleggame.common.Util.toastMessage
-import com.js.ghostleggame.data.database.GameResult
-import com.js.ghostleggame.data.dto.User
 import com.js.ghostleggame.databinding.ActivityMainBinding
 import com.js.ghostleggame.repo.DatabaseWithRoom
 import com.js.ghostleggame.repo.MySharedPreferences
 import com.js.ghostleggame.viewmodel.MainViewModel
-import org.koin.android.ext.android.bind
+import com.js.ghostleggame.viewmodel.MainViewModel.Companion.TAG
 import org.koin.android.ext.android.inject
-import java.util.Observer
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main), View.OnClickListener {
 
     private val mRoomDB: DatabaseWithRoom by inject()
     private val mPrefs: MySharedPreferences by inject()
     private val model: MainViewModel by viewModels()
 
+    override fun onClick(v: View?) {
+
+        when(v){
+            binding.tvPersonnelNum -> {
+                var dialog = Dialog(this)
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                dialog.setContentView(R.layout.number_picker_dialog)
+//                var _binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.number_picker_dialog, null, false);
+
+                dialog.show()
+
+//                val d = Dialog(this@MainActivity)
+//                d.setTitle("NumberPicker")
+//                d.setContentView(R.layout.number_picker_dialog)
+////                val b1: Button = d.findViewById<View>(R.id.button1) as Button
+////                val b2: Button = d.findViewById<View>(R.id.button2) as Button
+//                var b1 =
+//                val np =
+//
+//                np.maxValue = 100
+//                np.minValue = 0
+//                np.wrapSelectorWheel = false
+//                np.setOnValueChangedListener(this)
+//                b1.setOnClickListener(object : View.OnClickListener {
+//                    fun onClick(v: View?) {
+//                        tv.setText(np.value.toString())
+//                        d.dismiss()
+//                    }
+//                })
+//                b2.setOnClickListener(object : View.OnClickListener {
+//                    fun onClick(v: View?) {
+//                        d.dismiss()
+//                    }
+//                })
+//                d.show()
+
+
+                Log.e(TAG,"Test")
+            }
+
+            binding.tvWinningNum -> {
+                Log.e(TAG,"Test2")
+            }
+
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /**
+        * 초기 값 세팅
+        * */
+        binding.tvPersonnelNum.setOnClickListener(this)
+        binding.tvWinningNum.setOnClickListener(this)
 
         /**
         * 뷰모델에 라이브데이터랑 코인 연결하던중이였음
          *
          * https://www.youtube.com/watch?v=-b0VNKw_niY  21분 30초
         * */
-        model
 //        model = ViewModelProvider(this).get(MainViewModel::class.java)
 
 
@@ -59,12 +115,23 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
 
-
-
+    /**
+     * 사다리타기 맵 가져오기
+     * */
     fun getGhostLegBoard(): ArrayList<Int>{
 
 
         return arrayListOf()
     }
+
+    /**
+     * 해더 세팅
+     * */
+    fun setHeader(){
+
+//        binding.header. = getString(R.string.main_title)
+
+    }
+
 
 }
