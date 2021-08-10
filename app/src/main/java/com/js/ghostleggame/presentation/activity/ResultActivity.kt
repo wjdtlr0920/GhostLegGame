b3b7bc9d4a7be7a31e3a48e36b47429a3652b6d8
wjdtlr0920 <wjdtlr0920@gmail.com>
2021-08-11 00:09:22 +0900

    [김정식] 결과화면 리사이클러뷰 구성중

--- app/src/main/java/com/js/ghostleggame/presentation/activity/ResultActivity.kt
+++ app/src/main/java/com/js/ghostleggame/presentation/activity/ResultActivity.kt
@@ -2,17 +2,22 @@ package com.js.ghostleggame.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.js.ghostleggame.R
 import com.js.ghostleggame.databinding.ActivityResultBinding
+import com.js.ghostleggame.presentation.adapter.ResultAdapter
 
 
 class ResultActivity : BaseActivity<ActivityResultBinding>(R.layout.activity_result) {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+
+        binding.rvResult.layoutManager = LinearLayoutManager(this)
+        binding.rvResult.adapter = ResultAdapter(arrayOf("1123123123","2123234234234","31231342351234"))
+
+
     }
 }
---
+++ app/src/main/java/com/js/ghostleggame/presentation/adapter/ResultAdapter.kt
@@ -0,0 +1,44 @@
+package com.js.ghostleggame.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.js.ghostleggame.R
+
+
+class ResultAdapter(private val dataSet: Array<String>) :
+    RecyclerView.Adapter<ResultAdapter.ViewHolder>() {
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            textView = view.findViewById(R.id.textView)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.adapter_result, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = dataSet[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+
+}
