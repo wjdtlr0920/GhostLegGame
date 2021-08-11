7fdaf6cbf4a249435283646c3a8aa82d45124fce
[김정식] <wjdtlr0920@castfox.com>
2021-08-11 09:38:12 +0900

    [김정식] 개발중

--- app/src/main/java/com/js/ghostleggame/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/js/ghostleggame/presentation/activity/MainActivity.kt
@@ -10,6 +10,7 @@ import android.view.Window
 import android.widget.Button
 import android.widget.NumberPicker
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import com.js.ghostleggame.R
 import com.js.ghostleggame.databinding.ActivityMainBinding
@@ -26,9 +27,71 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main), 
     private val mPrefs: MySharedPreferences by inject()
     private val model: MainViewModel by viewModels()
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        /**
+         * 초기 값 세팅
+         * */
+        // 리스너
+        binding.tvPersonnelNum.setOnClickListener(this)
+        binding.tvWinningNum.setOnClickListener(this)
+        binding.btnReset.setOnClickListener(this)
+        binding.btnResult.setOnClickListener(this)
+        binding.btnAllStart.setOnClickListener(this)
+
+        binding.header.setting.setOnClickListener(this)
+
+        setHeader()
+
+        /**
+         * 뷰모델에 라이브데이터랑 코인 연결하던중이였음
+         *
+         * https://www.youtube.com/watch?v=-b0VNKw_niY  21분 30초
+         * */
+//        model = ViewModelProvider(this).get(MainViewModel::class.java)
+
+
+//        toastMessage(ctx!!, "Toast Test")
+
+//        mPrefs.prefsEditor.putInt("dark_mode", 6)
+//        mPrefs.prefsEditor.apply()
+
+//        mRoomDB.invoke().gameResultDao().insertGame(GameResult(17))
+//        var data = mRoomDB.invoke().gameResultDao().getAll()
+//        binding.tvTitle.text = data[2].date.toString()
+//        var data = mPrefs.prefs.getInt("dark_mode", 3)
+//        binding.user = User(1,"테스트",5,1)
+//        binding.lifecycleOwner
+//        binding.
+//
+//
+//        val model: MainViewModel by viewModels()
+//        model.getUsers().observe(this, Observer<List<User>>{ users ->
+//            // update UI
+//
+//
+//        })
+
+
+    }
+
+
     override fun onClick(v: View?) {
 
         when(v){
+
+
+            binding.header.back -> {
+                finish()
+            }
+            binding.header.setting -> {
+                var dialog = Dialog(this)
+                dialog.show()
+            }
+
+
             binding.tvPersonnelNum -> {
 //                var dialog = SelectDialog(this)
 //                dialog.show()
@@ -58,50 +121,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main), 
 
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        /**
-        * 초기 값 세팅
-        * */
-        binding.tvPersonnelNum.setOnClickListener(this)
-        binding.tvWinningNum.setOnClickListener(this)
-        binding.btnReset.setOnClickListener(this)
-        binding.btnResult.setOnClickListener(this)
-        binding.btnAllStart.setOnClickListener(this)
-
-        /**
-        * 뷰모델에 라이브데이터랑 코인 연결하던중이였음
-         *
-         * https://www.youtube.com/watch?v=-b0VNKw_niY  21분 30초
-        * */
-//        model = ViewModelProvider(this).get(MainViewModel::class.java)
-
-
-//        toastMessage(ctx!!, "Toast Test")
-
-//        mPrefs.prefsEditor.putInt("dark_mode", 6)
-//        mPrefs.prefsEditor.apply()
-
-//        mRoomDB.invoke().gameResultDao().insertGame(GameResult(17))
-//        var data = mRoomDB.invoke().gameResultDao().getAll()
-//        binding.tvTitle.text = data[2].date.toString()
-//        var data = mPrefs.prefs.getInt("dark_mode", 3)
-//        binding.user = User(1,"테스트",5,1)
-//        binding.lifecycleOwner
-//        binding.
-//
-//
-//        val model: MainViewModel by viewModels()
-//        model.getUsers().observe(this, Observer<List<User>>{ users ->
-//            // update UI
-//
-//
-//        })
-
-
-    }
-
 
     /**
      * 사다리타기 맵 가져오기
@@ -115,9 +134,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main), 
     /**
      * 해더 세팅
      * */
-    fun setHeader(){
+    private fun setHeader(){
 
-//        binding.header. = getString(R.string.main_title)
+        binding.header.title.text = getString(R.string.main_title)
+        binding.header.back.isVisible = false
 
     }
 
--- app/src/main/java/com/js/ghostleggame/presentation/activity/ResultActivity.kt
+++ app/src/main/java/com/js/ghostleggame/presentation/activity/ResultActivity.kt
@@ -1,16 +1,21 @@
 package com.js.ghostleggame.presentation.activity
 
+import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.annotation.LayoutRes
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.js.ghostleggame.R
 import com.js.ghostleggame.databinding.ActivityResultBinding
 import com.js.ghostleggame.presentation.adapter.ResultAdapter
+import com.js.ghostleggame.viewmodel.MainViewModel
 
 
-class ResultActivity : BaseActivity<ActivityResultBinding>(R.layout.activity_result) {
+class ResultActivity : BaseActivity<ActivityResultBinding>(R.layout.activity_result), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,38 @@ class ResultActivity : BaseActivity<ActivityResultBinding>(R.layout.activity_res
         binding.rvResult.adapter = ResultAdapter(arrayOf("1123123123","2123234234234","31231342351234"))
 
 
+
+        binding.header.back.setOnClickListener(this)
+        binding.header.setting.setOnClickListener(this)
+
+
+        setHeader()
+
     }
+
+
+    override fun onClick(v: View?) {
+
+        when(v) {
+            binding.header.back -> {
+                finish()
+            }
+            binding.header.setting -> {
+                var dialog = Dialog(this)
+                dialog.show()
+            }
+        }
+    }
+
+
+    /**
+     * 해더 세팅
+     * */
+    private fun setHeader(){
+
+        binding.header.title.text = getString(R.string.result_title)
+
+    }
+
+
 }
